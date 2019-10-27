eae05dd730ec00712a6a51f671c5461bd1f2fb89
Ian Phipps <phippsian@gmail.com>
2019-10-27 16:36:50 +0000

    Make cypress submit and improve the commands to run this

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,9 +1,11 @@
-describe('My First Test', function() {
+describe('Load the page, type some text and submit', function() {
   before(()=>{
     cy.visit('http://127.0.0.1:8080/');
   })
   it('Does not do much!', function() {
-    cy.get('input').type('abc').click('{enter}');
+    cy.get('input').type('abc');
+    cy.get('form').submit();
+    cy.get('ul li');
 
   })
 })
