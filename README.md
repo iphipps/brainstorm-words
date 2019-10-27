91580a9973821ca57842fb1f8bc74bcf8426207f
Ian Phipps <phippsian@gmail.com>
2019-10-27 16:53:38 +0000

    Use formdata

---
+++ README.md
@@ -0,0 +1,9 @@
+# Word Association
+for brainstorming
+
+##
+wip
+Write words as frantically as you can, hit enter and populate the page with your stream of consciousness.
+Go to vote move to vote on the importance of each individual word.
+
+
--- script.ts
+++ script.ts
@@ -1,10 +1,9 @@
-console.log('am i running yes')
+console.log('am i running yes');
 
-const onSubmit = (e) => {
+const onSubmit = (e: Event) => {
   e.preventDefault();
   console.log('e is', e);
-
-}
-
-
-
+  const inputData = new FormData(e.target as HTMLFormElement);
+  console.log(e.target);
+  console.log(inputData);
+};
