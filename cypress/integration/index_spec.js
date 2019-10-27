ee19b3e16bb312267dc41f03b9d218283e77196b
Ian Phipps <phippsian@gmail.com>
2019-10-27 17:52:56 +0000

    Use the template tag and make the li appear in different spots

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,11 +1,9 @@
 describe('Load the page, type some text and submit', function() {
-  before(()=>{
+  before(() => {
     cy.visit('http://127.0.0.1:8080/');
-  })
-  it('Does not do much!', function() {
+  });
+  it('Should reset the field after submit', () => {
     cy.get('input').type('abc');
     cy.get('form').submit();
-    cy.get('ul li');
-
-  })
-})
+  });
+});
--- script.ts
+++ script.ts
@@ -1,9 +1,42 @@
-console.log('am i running yes');
+console.log('hello');
+
+interface IWord {
+  word: string;
+  rating: number;
+  top: string;
+  left: string;
+}
+
+const store: IWord[] = [];
 
 const onSubmit = (e: Event) => {
   e.preventDefault();
   console.log('e is', e);
-  const inputData = new FormData(e.target as HTMLFormElement);
-  console.log(e.target);
-  console.log(inputData);
+  const target = e.target as HTMLFormElement;
+  const input = target[0] as HTMLInputElement;
+  const inputData = new FormData(target);
+  const data = {
+    word: inputData.get('word') as string,
+    rating: 0,
+    top: `${Math.floor(Math.random() * 100)}%`,
+    left: `${Math.floor(Math.random() * 100)}%`,
+  };
+  store.push(data);
+  console.log(data);
+  addToDom(data);
+  input.value = '';
+  console.log(store);
+};
+
+const addToDom = (word: IWord) => {
+  const template: HTMLTemplateElement = document.querySelector('template.li');
+  const content = template.content;
+  const span = content.querySelector('span');
+  const li = content.querySelector('li');
+  li.id = word.word;
+  li.style.left = word.left;
+  li.style.top = word.top;
+  span.textContent = word.word;
+  console.log(content);
+  document.querySelector('ul').appendChild(document.importNode(content, true));
 };
